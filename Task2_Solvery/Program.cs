f87fc7209db007b5a04c04a779e3079082203052
typespt <65303033+typespt@users.noreply.github.com>
2021-04-22 23:06:01 +0300

    LINQ part is done

--- Task2_Solvery/Program.cs
+++ Task2_Solvery/Program.cs
@@ -9,10 +9,35 @@ namespace Task2_Solvery
         static void Main(string[] args)
         {
             List<string> list = new List<string> { "Воронеж", "Коломна", "Луховицы", "Киев", "Москва",
-            "Воскресенск", "Бронницы", "Ульяновск", "Минск", "Молодечно", "Иваново", "Клин" };
+            "Воскресенск", "Бронницы", "Ульяновск", "Минск", "Молодечно", "Иваново", "Клин"};
 
-            var element = list.First(x => x.Length == 9);
+            var element = list.First(x => x.Length == 9); // длина города 9
             Console.WriteLine(element);
+
+            var element2 = list.FirstOrDefault(x => x.Contains("ъ")); // город содержит ъ
+            Console.WriteLine(element2);
+
+            var element3 = list.LastOrDefault(x => x.Length == 3); // последний город который содержит 3 буквы
+            Console.WriteLine(element3);
+
+            var element4 = list.Where(x => x.Contains('о'));  // все города, которые содержат о
+            foreach (var item in element4)
+            {
+                Console.Write(item + ' ');
+            }
+
+            Console.WriteLine();
+
+            var element5 = list.OrderByDescending(x => x.Length); // сортировка городов по убыванию
+            foreach (var item in element5)
+            {
+                Console.Write(item + ' ');
+            }
+
+            Console.WriteLine();
+
+            var element6 = list.Where(x => x.StartsWith('В')).OrderBy(x => x.Length).Last(); // начинаются на В сортировка по возрастанию берем последний
+            Console.WriteLine(element6);
         }
     }
 
