2f081734c9b6e77a101db72009fa1230a09ac11b
typespt <65303033+typespt@users.noreply.github.com>
2021-04-22 22:18:35 +0300

    Добавил первый город, который имеет 9 символов

--- Task2_Solvery/Program.cs
+++ Task2_Solvery/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Task2_Solvery
 {
@@ -6,7 +8,13 @@ namespace Task2_Solvery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string> { "Воронеж", "Коломна", "Луховицы", "Киев", "Москва",
+            "Воскресенск", "Бронницы", "Ульяновск", "Минск", "Молодечно", "Иваново", "Клин" };
+
+            var element = list.First(x => x.Length == 9);
+            Console.WriteLine(element);
         }
     }
+
+
 }
